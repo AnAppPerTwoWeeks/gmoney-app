c91a06280a3d427fb7949409a0101b9f6842ba45
mainendp <endp@kakao.com>
2020-04-27 23:14:29 +0900

    시작

---
+++ gmoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gmoney
+//
+//  Created by 장창순 on 27/04/2020.
+//  Copyright © 2020 AnAppPerTwoWeeks.DiaryApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
